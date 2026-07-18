94915a34dee8377a5e4ade2d0bcfb1e8599c5934
ramazanberber2801-ops <ramazanberber2801@gmail.com>
2026-07-18 16:59:57 +0300

    Use fixed customer portal

--- src/SiteRouter.tsx
+++ src/SiteRouter.tsx
@@ -1,5 +1,5 @@
 import type { MouseEvent, ReactNode } from 'react';
-import CustomerPortal from './CustomerPortal';
+import CustomerPortal from './CustomerPortalV2';
 import LegalPage from './LegalPages';
 import OnboardingPage from './OnboardingPage';
 import ModuleLibrary from './ModuleLibrary';
